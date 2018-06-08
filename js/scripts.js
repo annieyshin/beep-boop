ce6237ed029f18755d46bd1ab12fbafb4a2f40e9
Annie Shin <annieyshin@gmail.com>
2018-06-08 11:24:41 -0700

    add Js for numbers bigger than 100

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,3 @@
-var beepboop = function(input) {
-  if (input % 3 === 0) {
-    return true;
-  } else {
-    return false;
-  }
-};
 
 $(document).ready(function () {
   $("form#ourForm").submit(function(event) {
@@ -13,34 +6,49 @@ $(document).ready(function () {
     var result = beepboop(input);
     $("#result").text(result);
     $("#result").show();
-
-$("#result").text(input);
-
-if(result) {
-  $("#result").text("I'm sorry Dave, I can't do that");
-} else {
-  $("#result").text(input);
-}
-
-console.log("hello");
-
-
   });
 
 });
 
- //  var inputs = {"Beep": 1,
- //                 "Boop": 0,
- //                 };
- //  var number = "";
- //  var i = 0;
+    function beepboop(input) {
+      if (input > 100) {
+        return "That number is too large, beepity boop, try something smaller";
+      } else if (input % 3 === 0) {
+        return "I'm sorry, Dave. I'm afraid I can't do that";
+      }
+
+console.log("hello");
+
+// BEEP BOOP CONDITIONS
+  var inputs = {
+                                                      "Boop!": 100,
+                                                      "Beep!": 90,
+                                                      "Beep!": 80,
+                                                      "Beep!": 70,
+                                                      "Beep!": 60,
+                                                      "Beep!": 50,
+                                                      "Beep!": 40,
+                                                      "Beep!": 30,
+                                                      "Beep!": 20,
+                "I'm sorry, Dave. I'm afraid I can't do that": 18,
+                "I'm sorry, Dave. I'm afraid I can't do that": 15,
+                "I'm sorry, Dave. I'm afraid I can't do that": 12,
+                                                      "Beep!": 10,
+                "I'm sorry, Dave. I'm afraid I can't do that": 9,
+                "I'm sorry, Dave. I'm afraid I can't do that": 6,
+                "I'm sorry, Dave. I'm afraid I can't do that": 3,
+                                                      "Beep!": 1,
+                                                      "Boop!": 0, };
+
+  var output = "";
+  var i = 0;
  //
- //  for(i in numerals) {
- //    while(number >= numerals[i]) {
- //      roman += i;
- //      number -= numerals[i];
- //
- //    }
- //  }
- //  return roman;
- // }
+  for(i in inputs) {
+    while(input >= inputs[i]) {
+      output += i;
+      input -= inputs[i];
+
+    }
+  }
+  return output;
+ }
