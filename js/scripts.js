01a5dea71abf75d878d59a7870f6c1b0af778ba7
Annie Shin <annieyshin@gmail.com>
2018-06-08 11:43:31 -0700

    add Js for object with phrases for different numbers

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,15 @@
 $(document).ready(function () {
   $("form#ourForm").submit(function(event) {
     event.preventDefault();
-    var input = parseInt($("input#field").val());
-    var result = beepboop(input);
+    var input = parseInt($("#field").val());
+    var result = beepBoop(input);
     $("#result").text(result);
     $("#result").show();
   });
-
 });
 
-    function beepboop(input) {
-      if (input > 100) {
+    function beepBoop(number) {
+      if(number > 101) {
         return "That number is too large, beepity boop, try something smaller";
       } else if (input % 3 === 0) {
         return "I'm sorry, Dave. I'm afraid I can't do that";
@@ -20,8 +19,7 @@ $(document).ready(function () {
 console.log("hello");
 
 // BEEP BOOP CONDITIONS
-  var inputs = {
-                                                      "Boop!": 100,
+                                       var phrases = {"Boop!": 100,
                                                       "Beep!": 90,
                                                       "Beep!": 80,
                                                       "Beep!": 70,
@@ -40,13 +38,14 @@ console.log("hello");
                                                       "Beep!": 1,
                                                       "Boop!": 0, };
 
+
   var output = "";
   var i = 0;
- //
-  for(i in inputs) {
-    while(input >= inputs[i]) {
+
+  for(i in phrases) {
+    while(number >= phrases[i]) {
       output += i;
-      input -= inputs[i];
+      number -= phrases[i];
 
     }
   }
