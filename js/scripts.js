ffd8e7c0b546dd6b97b8b1706730c7f3d1f705df
Annie Shin <annieyshin@gmail.com>
2018-06-13 19:36:32 -0700

    initial second commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,41 @@
+var array = [];
+var results = [];
+
+var arrayPush = function(input) {
+
+}
 
 $(document).ready(function () {
-
   $("form#ourForm").submit(function(event) {
     event.preventDefault();
     var input = parseInt($("#field").val());
-    var result = beepBoop(input);
-    $("#result").text(result);
-    $("#result").show();
+    var string = (input).split.forEach();
+
+
+
+      CONSIDER .map function
+array.forEach(function(x) {
+  if (string.includes("1") {
+    results.push("Boop!");
+  }
+    else if (string.includes("0") {
+      result.push("Beep!");
+    }
+    else if (numberInput % 3 === 0) {
+      results.push("I'm sorry, Dave. I'm afraid I can't do that");
+  }
+    else {
+      results.push(string);
+    }
+
+})
+
+    $("#result").append("<li>" + [fill with output array].map? + "</li");
+
+
+
+
+
   });
 
 });
-
-
-function beepBoop(number) {
-  if(number > 12) {
-    return "That number is too large, beepity boop, try something smaller than 42";
-  } else if (number % 3 === 0) {
-    return "I'm sorry, Dave. I'm afraid I can't do that";
-    }
-
-
-    var numerals = {
-"Beep!": 0,
-      "Boop!": 1,
-      " 2 ": 2,
-      " I'm sorry, Dave. I'm afraid I can't do that ": 3,
-        " 4 ": 4,
-          " 5 ": 5,
-      "    I'm sorry, Dave. I'm afraid I can't do that ": 6,
-      " 7 ": 7,
-      " 8 ": 8,
-      "I'm sorry, Dave. I'm afraid I can't do that ": 9,
-         "Bleep!": 10,
-         " 11 ": 11,
-       " I'm sorry, Dave. I'm afraid I can't do that     ": 12};
-
-                  var code = "";
-          var i = 0;
-          for(i in numerals) {
-            while(number >= numerals[i]) {
-              code += i + " ";
-              i++;
-
-            }
-          }
-
-          return code;
-         }
