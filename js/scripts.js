9f2adf42c44629b855b5470f65759b71453107ed
Annie Shin <annieyshin@gmail.com>
2018-06-17 21:19:58 -0700

    added more Js to try to fix output

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,41 @@
 var array = [];
 var results = [];
 
+function blahboop(number) {
+for(var x = 0; x <= array.length; x++) {
+  if ( x % 3 === 0) {
+    results.push("I'm sorry, Dave. I'm afraid I can't do that");
+    } else if (x.toString().includes("1")) {
+      results.push("Beep!");
+    } else if (x.toString().includes("0")) {
+      results.push("Boop!");
+    }
+    else {
+      results.push(x);
+      }
 
+}
+}
 
 
 $(document).ready(function () {
   $("form#ourForm").submit(function(event) {
     event.preventDefault();
     var varInput = parseInt($("#submitField").val());
-    console.log(varInput);
-    // var numbToString   = varInput.toString();
-    // console.log(typeof(numbToString));
     var brokenDownNumb = countDown(varInput);
-    console.log(array);
     function countDown(number) {
       for(var i = 0; i <= varInput; i++) {
         array.push(i);
+        console.log(array);
+        var stringTheory = array.toString().split("");
       }
-    }
-    
+      var resultsMaybe = blahboop(stringTheory);
+
+            console.log(resultsMaybe);
+
+            }
+$("#result").text(results);
 
 
-
-//
-// array.forEach(function(x) {
-//   if (string.includes("1") {
-//     results.push("Boop!");
-//   }
-//     else if (string.includes("0") {
-//       result.push("Beep!");
-//     }
-//     else if (numberInput % 3 === 0) {
-//       results.push("I'm sorry, Dave. I'm afraid I can't do that");
-//   }
-//     else {
-//       results.push(string);
-//     }
-//
-// })
-//
-//     $("#result").append("<li>" + [fill with output array].map? + "</li");
-//
-//
-//
-//
-
-  });
-
+});
 });
